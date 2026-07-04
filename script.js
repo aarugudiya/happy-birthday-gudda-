3605ccea879e370a8e6cbcff678743021bbaa580
aarugudiya <notgonnaliy7@gmail.com>
2026-07-04 16:42:07 +0530

    Refactor message display and add scroll function

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
-function showMessage(){
+function scrollToLetter() {
+    document.getElementById("letter").scrollIntoView({
+        behavior: "smooth"
+    });
+}
 
-document.getElementById("message").innerHTML=`
+const message = `
 
-❤️ My Gudda...
+Dear Salman ❤️,
 
-Thank you for coming into my life.
+Happy Birthday to my Gudda...
+
+12 July will always be special because it gave this world my favourite person.
+
+Thank you for always supporting me.
+Thank you for making me stronger.
+Thank you for being my safe place.
+
+No matter how far we are...
 
 You'll always be my Raja Beta,
 my First Baby,
 my Salman.
 
-Happy Birthday ❤️
+If I could ask for just one birthday gift...
 
-Always Your Gudiya,
-Aaru Beti & Rani 🩷
+It would be one tight huggie from you. 🫂
+
+I Love You.
+Miss You. 🥺
+
+Always Yours,
+
+Aaru Beti
+Your Gudiya
+Your Rani ❤️
 
 `;
 
+let i = 0;
+
+function typeWriter() {
+    if (i < message.length) {
+        document.getElementById("typewriter").innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 40);
+    }
 }
+
+window.onload = typeWriter;
